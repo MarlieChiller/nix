133373edff408e8cee295dd29fa693411449d222
MarlieChiller <charlie.miller@zego.com>
2025-09-02 14:40:29 +0100

    Flake update

--- home-manager/packages/ssh/default.nix
+++ home-manager/packages/ssh/default.nix
@@ -1,19 +1,18 @@
 {...}: {
   programs.ssh = {
     enable = true;
-    forwardAgent = true;
-    serverAliveInterval = 60;
-    controlMaster = "auto";
-    controlPersist = "30m";
     extraConfig = ''
       IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
     '';
     matchBlocks = {
       "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        forwardAgent = false;
+        controlMaster = "auto";
+        controlPersist = "30m";
         extraOptions = {preferredAuthentications = "publickey";};
+        forwardAgent = false;
+        hostname = "github.com";
+        serverAliveInterval = 60;
+        user = "git";
       };
     };
   };
