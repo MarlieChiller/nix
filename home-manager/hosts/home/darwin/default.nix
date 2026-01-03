52fd8d158ed5d837b5335eb97c48caf2421ec47e
MarlieChiller <marliechiller@proton.me>
2026-01-03 15:23:01 +0000

    Photo editing

--- home-manager/hosts/home/darwin/default.nix
+++ home-manager/hosts/home/darwin/default.nix
@@ -1,9 +1,19 @@
-{userConfig, ...}: {
+{
+  userConfig,
+  pkgs,
+  ...
+}: {
   # Packages and userConfig passed from flake level
 
   home = {
     username = userConfig.username;
     homeDirectory = "/Users/${userConfig.username}";
     stateVersion = "23.11"; # Please read the comment before changing.
+
+    packages = with pkgs; [
+      # Photo editing
+      darktable
+      krita
+    ];
   };
 }
--- home-manager/hosts/home/nixos/default.nix
+++ home-manager/hosts/home/nixos/default.nix
@@ -1,9 +1,19 @@
-{userConfig, ...}: {
+{
+  userConfig,
+  pkgs,
+  ...
+}: {
   # Packages and userConfig passed from flake level
 
   home = {
     username = userConfig.username;
     homeDirectory = "/home/${userConfig.username}";
     stateVersion = "24.11"; # Please read the comment before changing.
+
+    packages = with pkgs; [
+      # Photo editing
+      darktable
+      krita
+    ];
   };
 }
